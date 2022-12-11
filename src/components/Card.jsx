175dd90aee11707775d1757f2680a25e5c54cb95
nmesoma <nmesomasolo2809@gmail.com>
2022-12-11 08:25:59 +0100

    add one card only

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import './Card.css'
-import CheckIcon from '@mui/icons-material/Check';
+// import React from 'react'
+// import './Card.css'
+// import CheckIcon from '@mui/icons-material/Check';
 
-function Card(props) {
-  return (
-    <div className='kard me-5 mt-5'>
-      <div>
-        {/* <h3 className='mt-3 ms-3 me-3 introh3'><img src={props.img} alt={props.title}/>{props.title}<sup>{props.sup}</sup></h3> */}
-        <h3 className='mt-3 ms-3 me-3 introh3'><img src={props.img} alt={props.title}/>{props.title}<sup>{props.sup}</sup></h3>
-        <p className='mt-3 ms-3 me-3 pb-3'>{props.para}</p>
-        <p>{props.img} </p>
-      </div>
-      <div className='cardb mt-3 ms-3 me-3 mb-3'>
-        <h4 className='cardbh4 pt-3 pb-4'>Popular features</h4>
-        <p> <CheckIcon className='vector' />{props.l1}</p>
-        <p><CheckIcon className='vector' /> {props.l2}</p>
-        <p><CheckIcon className='vector' /> {props.l3}</p>
-      </div>
-      <div className="d-grid gap-2 ms-3 me-3 mt-3 mb-3 " >
-        <button className="btn btn-primary text-white" type="button">Get Started</button>
-      </div>
-    </div>
+// function Card(props) {
+//   return (
+//     <div className='kard me-5 mt-5'>
+//       <div>
+//         {/* <h3 className='mt-3 ms-3 me-3 introh3'><img src={props.img} alt={props.title}/>{props.title}<sup>{props.sup}</sup></h3> */}
+//         <h3 className='mt-3 ms-3 me-3 introh3'><img src={props.img} alt={props.title}/>{props.title}<sup>{props.sup}</sup></h3>
+//         <p className='mt-3 ms-3 me-3 pb-3'>{props.para}</p>
+//         <p>{props.img} </p>
+//       </div>
+//       <div className='cardb mt-3 ms-3 me-3 mb-3'>
+//         <h4 className='cardbh4 pt-3 pb-4'>Popular features</h4>
+//         <p> <CheckIcon className='vector' />{props.l1}</p>
+//         <p><CheckIcon className='vector' /> {props.l2}</p>
+//         <p><CheckIcon className='vector' /> {props.l3}</p>
+//       </div>
+//       <div className="d-grid gap-2 ms-3 me-3 mt-3 mb-3 " >
+//         <button className="btn btn-primary text-white" type="button">Get Started</button>
+//       </div>
+//     </div>
 
-  )
-}
-export default Card
+//   )
+// }
+// export default Card
--- src/components/Cardrow.jsx
+++ src/components/Cardrow.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import './Cardrow.css'
-import Card from './Card'
+// import Card from './Card'
 import contents from './cardcontent'
+import CheckIcon from '@mui/icons-material/Check';
+import './Card.css'
+import MarketingHub_Icon  from '../assets/images/MarketingHub_Icon.png';
 
-const createCard = (content)=>{
-    return <Card 
-    key={content.id}
-    img={content.img}
-    title={content.title}
-    sup={content.sup}
-    para={content.para}
-    l1={content.l1}
-    l2={content.l2}
-    l3={content.l3}
-     />
-}
+
+// const createCard = (content) => {
+//     return <Card
+//         key={content.id}
+//         img={content.img}
+//         title={content.title}
+//         sup={content.sup}
+//         para={content.para}
+//         l1={content.l1}
+//         l2={content.l2}
+//         l3={content.l3}
+//     />
+// }
 
 function Cardrow() {
     return (
         <div className='container-fluid cardrow' >
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 cardrowpad ">
-            {contents.map(createCard)}
-            {/* <Card img={contents[0].Img} /> 
+                {/* {contents.map(createCard)} */}
+                {/* <Card img={contents[0].Img} /> 
              <Card />
             <Card />
             <Card />
             <Card />  */}
+                <div className='kard me-5 mt-5'>
+                    <div>
+                        <h3 className='mt-3 ms-3 me-3 introh3'><img src={MarketingHub_Icon} alt={contents[0].title} />{contents[0].title}<sup>{contents[0].sup}</sup></h3>
+                        <p className='mt-3 ms-3 me-3 pb-3'>{contents[0].para}</p>
+                    </div>
+                    <div className='cardb mt-3 ms-3 me-3 mb-3'>
+                        <h4 className='cardbh4 pt-3 pb-4'>Popular features</h4>
+                        <p> <CheckIcon className='vector' />{contents[0].l1}</p>
+                        <p><CheckIcon className='vector' /> {contents[0].l2}</p>
+                        <p><CheckIcon className='vector' /> {contents[0].l3}</p>
+                    </div>
+                    <div className="d-grid gap-2 ms-3 me-3 mt-3 mb-3 " >
+                        <button className="btn btn-primary text-white" type="button">Get Started</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
